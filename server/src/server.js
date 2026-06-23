a26b9f7d076793e33aa94d050a0df1a1ae016bbf
Joseph Scott-Bentley <jscottbentley@hotmail.com>
2026-06-23 11:22:48 +0100

    Theme updated to wimbledon green

--- server/src/server.js
+++ server/src/server.js
@@ -5,7 +5,7 @@ const db = require("./database/db");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // load schema
 const schemaPath = path.join(__dirname, "database", "schema.sql");
